54eda58d990aa2bfcfa74a59461fdf9795602786
Tim Reed <reed11tim@gmail.com>
2024-01-31 23:19:10 -0500

    fixed navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -40,12 +40,26 @@
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
       </svg>
     </button>
+    <!-- Desktop Nav Links -->
+    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+        <li>
+          <a href="/" class="block py-2 px-3 text-rose-800 rounded md:bg-transparent md:text-rose-800 md:p-0" aria-current="page">Home</a>
+        </li>
+        <li>
+          <a href="/about" class="block py-2 px-3 text-rose-800 rounded md:bg-transparent md:text-rose-800 md:p-0">About</a>
+        </li>
+        <li>
+          <a href="/contact" class="block py-2 px-3 text-rose-800 rounded md:bg-transparent md:text-rose-800 md:p-0">Contact</a>
+        </li>
+      </ul>
+    </div>
   </div>
 
   <!-- Backdrop -->
   <div class={`fixed inset-0 z-20 ${isMenuOpen ? 'bg-black opacity-50' : 'opacity-0 pointer-events-none'} transition-opacity duration-300 ease-in-out`} on:click={toggleMenu} on:keydown={handleKeydown} role="button" aria-pressed={isMenuOpen ? 'true' : 'false'} tabindex="0"></div>
   
-  <!-- Full screen menu -->
+  <!-- Full screen menu for mobile -->
   <div class={`fixed inset-0 z-30 ${isMenuOpen ? styles.menuEnterActive : styles.menuExitActive} transform transition-transform duration-300 ease-in-out bg-rose-50`}>
     <div class="flex flex-col h-full">
       <!-- Close Button -->
